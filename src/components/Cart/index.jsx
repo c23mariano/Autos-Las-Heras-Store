6764541ab1390d3bade3a85799d33db9ab962b45
Mariano Carrique <marianocarrique@gmail.com>
2023-10-04 22:55:20 -0300

    Proyecto final

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/cartContext";
 import ItemCart from "../ItemCart";
@@ -6,6 +7,24 @@ import ItemCart from "../ItemCart";
 const Cart = () => {
 
     const { cart, totalPrice } = useCartContext();
+
+    const order = {
+        buyer: {
+            name: 'Mariano',
+            email: 'mariano@gmail.com',
+            phone: '1122334455',
+            adress: 'Directorio 32'
+        },
+        items: cart.map(product => ({id: product.id, title: product.title, price: product.price, quantity: product.quantity })),
+        total: totalPrice(),
+    }
+
+    const handleClick = () => {
+        const db = getFirestore();
+        const ordersCollection = collection(db, 'orders');
+        addDoc(ordersCollection, order)
+        .then(({id}) => console.log(id))
+    }
 
     if (cart.length === 0) {
         return (
@@ -24,6 +43,7 @@ const Cart = () => {
             <p>
                 total: {totalPrice()}
             </p>
+            <button onClick={handleClick}>Emitir orden de compra</button>
         </>
     );
 }
